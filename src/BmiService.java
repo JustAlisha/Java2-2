feb136d54630d1b63c311c08e260c9a86b29b0fa
Test <bloodreighn965@gmail.com>
2024-03-23 00:15:20 +0300

    first commit

---
+++ src/BmiService.java
@@ -0,0 +1,9 @@
+public class BmiService {
+    public int calcalate(int weightInKg , double heightInMeters){
+
+    double bmi = weightInKg/ heightInMeters /heightInMeters);
+    return (int) bmi;
+
+    }
+
+}
