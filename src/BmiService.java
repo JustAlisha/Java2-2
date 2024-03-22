00ab7ae0bc39cc4bbd7be080734e5387ef8f3edd
Test <bloodreighn965@gmail.com>
2024-03-23 00:19:34 +0300

    First

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
-    public int calcalate(int weightInKg , double heightInMeters){
+    public int calcalate(int weightInKg, double heightInMeters) {
 
-    double bmi = weightInKg/ heightInMeters /heightInMeters);
-    return (int) bmi;
+        double bmi = weightInKg / heightInMeters / heightInMeters);
+        return (int) bmi;
 
     }
 
